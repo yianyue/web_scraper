b04f1506c0c729a3b15fc09abf69b690a45f3181
yianyue <yianyue@gmail.com>
2015-07-08 13:54:50 -0400

    created comment class. changed main.rb to hn_scraper.rb

---
+++ comment.rb
@@ -0,0 +1,11 @@
+class Comment
+  def initialize
+    @user_name
+    @time
+    @content
+    # comment level is tracked by width of the image s.gif wrapped in td class = "ind"
+    # 0, 40, 80 ...
+    @level
+  end
+
+end
